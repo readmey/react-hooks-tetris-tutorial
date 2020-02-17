58cf0de659526e096d65390d15e55e489a41e78b
readmey <mytrang.hong@hotmail.de>
2020-02-17 08:20:22 +0100

    update landing page with tetris start screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import tetrisBackground from "./assets/images/tetris-bg.jpg";
 
 function App() {
+  const TetrisWrapper = styled.div`
+    background-image: url(${tetrisBackground});
+    width: 100vw;
+    height: 100vh;
+    background-size: contain;
+    background-position: center center;
+  `;
+
+  const Button = styled.button`
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    padding: 1rem 2rem;
+    background-color: #4dc4f2;
+    border: none;
+    color: white;
+    font-size: 2rem;
+    font-weight: bold;
+    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
+  `;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TetrisWrapper>
+        <Button>Start the Game</Button>
+      </TetrisWrapper>
     </div>
   );
 }
