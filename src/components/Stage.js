ae9497bed4819c1dcca3d49583ee77bf8674296f
readmey <mytrang.hong@hotmail.de>
2020-03-03 18:50:50 +0100

    add tetris components: cell, display, stage and customButton

---
+++ src/components/Stage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import Cell from "./Cell";
+
+export const StyledStage = styled.div`
+  display: grid;
+  grid-template-rows: repeat(
+    ${props => props.height},
+    calc(25vw / ${props => props.width})
+  );
+  grid-template-columns: repeat(${props => props.width}, 1fr);
+  grid-gap: 1px;
+  border: 2px solid #4f4f4f;
+  width: 100%;
+  max-width: 25vw;
+  background: #4f4f4f;
+`;
+
+const Stage = ({ stage }) => (
+  <StyledStage width={stage[0].length} height={stage.length}>
+    {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+  </StyledStage>
+);
+
+export default Stage;
