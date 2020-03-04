f413317775a8675f79889ba9df5c26e174fcdc26
My Trang Hong <mytrang.hong@prodyna.com>
2020-03-04 20:38:37 +0100

    Updates

---
+++ precache-manifest.72123aff0297cbe1808241b38dc10395.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f880d0f0d15e30c2bed920b7f2afb868",
+    "url": "/react-hooks-tetris-tutorial/index.html"
+  },
+  {
+    "revision": "d27f6157894440a7c8bc",
+    "url": "/react-hooks-tetris-tutorial/static/css/main.8d462d85.chunk.css"
+  },
+  {
+    "revision": "e9c2705f72ea42f6f6db",
+    "url": "/react-hooks-tetris-tutorial/static/js/2.5c2ad9e8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-hooks-tetris-tutorial/static/js/2.5c2ad9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27f6157894440a7c8bc",
+    "url": "/react-hooks-tetris-tutorial/static/js/main.781ace3e.chunk.js"
+  },
+  {
+    "revision": "7751585b5c6f3c41eb1d",
+    "url": "/react-hooks-tetris-tutorial/static/js/runtime-main.2896c87e.js"
+  },
+  {
+    "revision": "03ce106b21a39c5dd063492d1966bc18",
+    "url": "/react-hooks-tetris-tutorial/static/media/404-robot-icon.03ce106b.svg"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-hooks-tetris-tutorial/static/media/Pixel.e2897552.woff"
+  },
+  {
+    "revision": "4ae866dc425a9904e84252322ec8413b",
+    "url": "/react-hooks-tetris-tutorial/static/media/Tetris.4ae866dc.mp3"
+  },
+  {
+    "revision": "9bc0c73c72523c4e933bc3a7b8d86b3b",
+    "url": "/react-hooks-tetris-tutorial/static/media/TetrisDrop.9bc0c73c.wav"
+  },
+  {
+    "revision": "7e0750ef5809d3e9169cc52875c256ed",
+    "url": "/react-hooks-tetris-tutorial/static/media/TetrisGameover.7e0750ef.wav"
+  },
+  {
+    "revision": "df5e003847b43388529823eef3d105bf",
+    "url": "/react-hooks-tetris-tutorial/static/media/game-bg.df5e0038.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ce40ba87fea73b72571525d6cbe42ab.js"
+  "/react-hooks-tetris-tutorial/precache-manifest.72123aff0297cbe1808241b38dc10395.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-hooks-tetris-tutorial/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
