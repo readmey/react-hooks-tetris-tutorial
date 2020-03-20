4a12be263a1d4c1c241cfd511815ecae00bc8f20
readmey <60737926+readmey@users.noreply.github.com>
2020-03-20 15:46:45 +0000

    Deploying to gh-pages from  - 97dc1ec63acee9a64df9405682a4a4ce75910973 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-hooks-tetris-tutorial/precache-manifest.a48de442a45c5445967530aa69f3cc02.js"
+  "/react-hooks-tetris-tutorial/precache-manifest.93046b92ac41d82c14890658f563155e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-hooks-tetris-tutorial/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
