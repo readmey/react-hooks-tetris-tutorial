00bcbc1a3821ce9bdea7586b1c340f5a33560119
readmey <mytrang.hong@hotmail.de>
2020-03-20 16:51:17 +0100

    update component styles

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,19 +2,15 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledDisplay = styled.div`
-  box-sizing: border-box;
-  margin: 0 0 20px 0;
-  padding: 20px;
-  border: 4px solid #dbeaff;
-  border-radius: 20px;
+  margin-bottom: 1rem;
+  padding: 0.8rem;
+  height: 10px;
+  border: 2px solid #dbeaff;
+  border-radius: 15px;
   color: ${props => (props.gameOver ? "red" : "#999")};
   background: #000;
-  font-size: 1rem;
   @media (max-width: 992px) {
     font-size: 0.6rem;
-    padding: 0.8rem 0.5rem;
-    min-width: 78px;
-    height: 38px;
     margin-right: 5px;
   }
 `;
