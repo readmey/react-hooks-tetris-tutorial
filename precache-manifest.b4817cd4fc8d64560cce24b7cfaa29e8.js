a7b39737d104624fa12c905f3f12696e3d2b76d6
My Trang Hong <mytrang.hong@prodyna.com>
2020-03-22 16:05:12 +0100

    Updates

--- precache-manifest.b4817cd4fc8d64560cce24b7cfaa29e8.js
+++ precache-manifest.b4817cd4fc8d64560cce24b7cfaa29e8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "37f0e8a466cea8126e852b63d1b6acd0",
+    "revision": "f38962ad3cf02aad793c5da1375517bd",
     "url": "/react-hooks-tetris-tutorial/index.html"
   },
   {
-    "revision": "d9aa13d7ca340ec544a7",
+    "revision": "fe8118894c184423c360",
     "url": "/react-hooks-tetris-tutorial/static/css/main.d3ac9262.chunk.css"
   },
   {
-    "revision": "dabf08a0750b1f0bc4ac",
-    "url": "/react-hooks-tetris-tutorial/static/js/2.4864cfcc.chunk.js"
+    "revision": "21f3df54286333683e1b",
+    "url": "/react-hooks-tetris-tutorial/static/js/2.6a853e6b.chunk.js"
   },
   {
-    "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/react-hooks-tetris-tutorial/static/js/2.4864cfcc.chunk.js.LICENSE.txt"
+    "revision": "61e9854a6bf57dc077cc06fd0e1da9b1",
+    "url": "/react-hooks-tetris-tutorial/static/js/2.6a853e6b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d9aa13d7ca340ec544a7",
-    "url": "/react-hooks-tetris-tutorial/static/js/main.08c0dbc3.chunk.js"
+    "revision": "fe8118894c184423c360",
+    "url": "/react-hooks-tetris-tutorial/static/js/main.e86e0176.chunk.js"
   },
   {
     "revision": "7751585b5c6f3c41eb1d",
@@ -44,6 +44,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-hooks-tetris-tutorial/static/media/TetrisGameover.7e0750ef.wav"
   },
   {
+    "revision": "f9bb23c36e389843d261017748c8bf3a",
+    "url": "/react-hooks-tetris-tutorial/static/media/crown.f9bb23c3.svg"
+  },
+  {
     "revision": "df5e003847b43388529823eef3d105bf",
     "url": "/react-hooks-tetris-tutorial/static/media/game-bg.df5e0038.svg"
   },
@@ -62,5 +66,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "6f3f8acf152f3f9fde021ed4e04d1556",
     "url": "/react-hooks-tetris-tutorial/static/media/keyUp.6f3f8acf.svg"
+  },
+  {
+    "revision": "a642fe5b1e939e6b45b7b8e9c5d63274",
+    "url": "/react-hooks-tetris-tutorial/static/media/poop.a642fe5b.svg"
   }
 ]);
